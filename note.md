9758b8148568b09d5d8d843aa8a8456af11f832d
LJlkdskdjflsa <liliangjya@gmail.com>
2022-07-26 15:41:05 +0800

    feat: first commit

---
+++ note.md
@@ -0,0 +1,3 @@
+先改 UI
+- /Users/lj/Documents/Work/NCHU/OnChainFund/interface/src/layout
+  - Sidebar 刪除用不掉的按鈕
--- src/layout/Sidebar/MenuLinks.tsx
+++ src/layout/Sidebar/MenuLinks.tsx
@@ -3,7 +3,7 @@ import { MENU_LINK } from 'src/constants'
 import { ThemeContext } from 'styled-components'
 import { Menu, MenuItem, MenuLink, MenuName, MenuExternalLink, MenuWrapper } from './styled'
 import { Box, Text, useTranslation } from '@pangolindex/components'
-import { Dashboard, Swap, Stake, Pool, Buy, Vote, Airdrop } from '../../components/Icons'
+import { Dashboard, Swap, Pool } from '../../components/Icons'
 import Charts from '../../assets/svg/menu/analytics.svg'
 import { ANALYTICS_PAGE } from '../../constants'
 import Bridge from '../../assets/svg/menu/bridge.svg'
@@ -48,40 +48,11 @@ export const MenuLinks: React.FC<Props> = ({ collapsed = false, onClick }) => {
       isActive: location?.pathname?.startsWith(MENU_LINK.swap)
     },
     {
-      link: MENU_LINK.buy,
-      icon: Buy,
-      title: t('header.buy'),
-      id: 'buy',
-      isActive: location?.pathname?.startsWith(MENU_LINK.buy)
-    },
-    {
       link: MENU_LINK.pool,
       icon: Pool,
       title: `${t('header.pool')} & ${t('header.farm')}`,
       id: 'pool',
       isActive: location?.pathname?.startsWith(MENU_LINK.pool)
-    },
-    {
-      link: `${MENU_LINK.stake}/0`,
-      icon: Stake,
-      title: t('header.stake'),
-      id: 'stake',
-      isActive: location?.pathname?.startsWith(MENU_LINK.stake)
-    },
-
-    {
-      link: MENU_LINK.vote,
-      icon: Vote,
-      title: t('header.vote'),
-      id: 'vote',
-      isActive: location?.pathname?.startsWith(MENU_LINK.vote)
-    },
-    {
-      link: MENU_LINK.airdrop,
-      icon: Airdrop,
-      title: 'Airdrop',
-      id: 'airdrop',
-      isActive: location?.pathname?.startsWith(MENU_LINK.airdrop)
     }
   ]
 
